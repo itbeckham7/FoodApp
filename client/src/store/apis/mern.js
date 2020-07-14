8f491e1028a54585f5a677a70f7b90a031247c6a
itbeckham7 <itbeckham7@hotmail.com>
2020-07-14 22:32:23 +0800

    0714 - First Deployment
    First Deployment

--- client/src/store/apis/mern.js
+++ client/src/store/apis/mern.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import config from '../../config'
 
 const mernApi = axios.create();
 
 // Set your own baseURL
-// mernApi.defaults.baseURL = 'http://localhost:8000';
+mernApi.defaults.baseURL = config.serverUrl;
 
 mernApi.setAuthToken = (jwtToken) => {
   mernApi.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
